4890aba6347c0d15d194f5a887d38583c09389d4
nurirfansyah <40015347+nurirfansyah@users.noreply.github.com>
2018-09-29 12:30:09 +0700

    Add files via upload

---
+++ GUI GCS/webcamgui.py
@@ -0,0 +1,44 @@
+import tkinter as tk
+from tkinter import *
+import cv2
+from PIL import Image, ImageTk
+
+#width, height = 800, 600
+cap = cv2.VideoCapture(0)
+#cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+#cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+
+root = tk.Tk()
+root.bind('<Escape>', lambda e: root.quit())
+lmain_vid = tk.Label(root, relief=RIDGE)
+lmain_map = tk.Label(text = 'Nanti peta di sini')
+lmain_info = tk.Label(text = 'Data telemetri:\nLon=\nLat=',relief=RIDGE,justify=LEFT)
+#lmain.pack()
+lmain_vid.grid(row=0, column = 0)
+lmain_map.grid(row=0, column = 1)
+lmain_info.grid(row=1, column = 0)
+
+def show_frame():
+    _, frame = cap.read()
+    frame = cv2.flip(frame, 1)
+    cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    img = Image.fromarray(cv2image)
+    imgtk = ImageTk.PhotoImage(image=img)
+    lmain_vid.imgtk = imgtk
+    lmain_vid.configure(image=imgtk)
+    lmain_vid.after(10, show_frame)
+
+
+    img2 = Image.fromarray(gray)
+    #imgtk2 = ImageTk.PhotoImage(image=img2)
+    #lmain2.imgtk = imgtk2
+    # lmain2.configure(image=imgtk2)
+    # lmain2.after(10, show_frame)
+
+
+show_frame()
+root.mainloop()
