18705643d1ffa8669154a89f8c12aa983be03cf8
Aldinata Rizky Revanda <5116100023@mahasiswa.integra.its.ac.id>
2018-09-29 12:46:29 +0700

    mencoba

--- GUI GCS/aldi_nyoba.py
+++ GUI GCS/aldi_nyoba.py
@@ -1,9 +1,13 @@
 # Trial Project
 from tkinter import *
+from PIL import ImageTk, Image
 
 ### main
-window = tk()
+window = Tk()
 window.title("My Project")
+### my photo
+photo1 = ImageTk.PhotoImage(Image.open("bayucaraka.png"))
+Label(window, image=photo1, bg="black").grid(row=0, column=0, sticky=E)
 
-## run the main loop
+### run the main loop
 window.mainloop()
